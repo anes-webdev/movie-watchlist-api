9a913417526d1f890ae005f40cb4a59392360c2f
anes-webdev <anes.webdev@gmail.com>
2026-05-22 15:28:32 +0330

    feat: add zod validation to watch list api

--- src/routes/watchListRoutes.js
+++ src/routes/watchListRoutes.js
@@ -1,12 +1,19 @@
 import express from "express";
-import { addToWatchList, getAllWatchLists, removeFromWatchList } from "../controllers/watchListController.js";
+import {
+  addToWatchList,
+  getAllWatchLists,
+  removeFromWatchList,
+} from "../controllers/watchListController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { validateRequest } from "../middleware/validateRequest.js";
+import { addToWatchListSchema } from "../validators/watchlistValidators.js";
 const router = express.Router();
 
 router.use(authMiddleware);
-router.post("/", addToWatchList);
+// We validate request body before sending query to DB:
+router.post("/", validateRequest(addToWatchListSchema), addToWatchList);
 router.get("/", getAllWatchLists);
-// With params: 
+// With params:
 router.delete("/:id", removeFromWatchList);
 
 export default router;
